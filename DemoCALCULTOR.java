81fe5286bc6b786f3932d5112f82309c47c1e926
ksaini-web <kartiksaini9084@gmail.com>
2026-03-24 14:10:00 +0530

    Add DemoCALCULTOR class with lambda calculator

---
+++ DemoCALCULTOR.java
@@ -0,0 +1,13 @@
+package lambda;
+
+public class DemoCALCULTOR {
+	
+	public static void main(String[] args) {
+		
+		  demo2 d = (x,y)->{return x+y;};
+		
+		
+		System.out.println(d.CALCULTOR(24,23));
+	}
+
+}
