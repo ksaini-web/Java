152acb4c68802aa92168a103a046c6f5498f1922
ksaini-web <kartiksaini9084@gmail.com>
2026-03-31 17:56:22 +0530

    Rename class DEMO to DEMOStack

---
+++ DEMOStack.java
@@ -0,0 +1,28 @@
+package Ducatjava;
+
+import java.util.Stack;
+
+public class DEMOStack {
+	
+	public static void main(String[] args) {
+
+	Stack<Integer> s = new Stack<>();
+	
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	
+	System.out.println(s);
+	
+	s.pop();
+
+	System.out.println(s);
+	
+
+	System.out.println(s.peek());
+	
+	
+	
+	}
+	
+}
