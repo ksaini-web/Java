55c4633e66e3bb9d83075d591ebefdac14c49b71
ksaini-web <kartiksaini9084@gmail.com>
2026-04-02 20:28:30 +0530

    Add Count class to count occurrences of numbers

---
+++ Count.java
@@ -0,0 +1,18 @@
+
+package collection;
+
+import java.util.HashMap;
+
+public class Count {
+	public static void main(String[] args) {
+
+	int [] arr = {1,2,3,4,5,1,2};
+	
+	HashMap<Integer,Integer> map = new HashMap<>();
+	
+	for(int num : arr){
+		map.put(num,map.getOrDefault(num,0)+1);
+	}
+	System.out.print(map);
+	}
+}
